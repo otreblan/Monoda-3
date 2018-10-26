0b5447d1d42a5ae6a5d3b70188f60f7f9843a219
otreblan <otreblain@gmail.com>
2018-10-25 19:42:48 -0500

    primer paso
    un cuadrado en un fondo rojo y negro

---
+++ Monoda eR/Assets/Scripts/plaScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plaScript : MonoBehaviour {
+
+	public float vel = 1f;
+
+	Vector3 distan;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distan = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0f),1.0f)*(Time.deltaTime*vel);;
+		transform.position += distan;
+	}
+}
