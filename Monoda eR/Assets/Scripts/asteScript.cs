c441c21e4a4e1454ab27ea3902ea6b0d142dbb11
otreblan <otreblain@gmail.com>
2018-10-27 07:56:53 -0500

    Ahora tiene un asteroide

---
+++ Monoda eR/Assets/Scripts/asteScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteScript : MonoBehaviour {
+	
+	public float rotVel = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0f,0f,Time.deltaTime*rotVel,Space.World);
+	}
+}
