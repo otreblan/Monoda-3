cbcbae6a9a33be6dc2450a631008136334b8884d
otreblan <otreblain@gmail.com>
2018-10-28 09:06:24 -0500

    Ahora el fondo es más lento

---
+++ Monoda eR/Assets/Scripts/fondoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fondoScript : MonoBehaviour {
+
+	public Transform sigue; //a lo que sigue
+
+	[Range(1.0f, 2.0f)]
+	public float magnitud; //Cuanto se moverá(funciona mejor entre 1.0 y 2.0)
+
+	/// <summary>
+	/// LateUpdate is called every frame, if the Behaviour is enabled.
+	/// It is called after all Update functions have been called.
+	/// </summary>
+	void LateUpdate()
+	{
+		transform.position = new Vector3(sigue.position.x,sigue.position.y,transform.position.z)/magnitud;
+		//transform.localPosition = sigue.position/magnitud;
+	}
+}
