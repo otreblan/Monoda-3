36f34d3bcd78642e3cf83492587f39c6e9210661
otreblan <otreblain@gmail.com>
2018-11-04 09:30:19 -0500

    0.0.2
    Está bugeado. pero ahora tiene un menú

---
+++ Monoda vS/Assets/Scripts/spawnScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class spawnScript : MonoBehaviour {
+
+	public GameObject player;
+	// Use this for initialization
+	public void spawnPlayer()
+	{
+		StartCoroutine(coSpawnPlayer());
+	}
+
+	IEnumerator coSpawnPlayer()
+	{
+		yield return new WaitForSecondsRealtime(1.0f);
+		GameObject pla = Instantiate(player);
+		
+		yield return null;
+	}
+}
---
+++ Monoda vS/Assets/Scripts/vidaScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidaScript : MonoBehaviour {
+
+	public float vidaMax;
+	public float vidaInicial;
+	public float Vida
+	{
+		get
+		{
+			return vida;
+		}
+		set
+		{
+			if (vida <= 0f)
+			{
+				//destruir
+			}
+			vida = value < vidaMax ? value : vidaMax;
+		}
+	}
+	float vida;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
