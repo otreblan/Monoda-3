2ac703b370543fcf5b5326e1e65bbb18c5a804f0
otreblan <otreblain@gmail.com>
2018-11-04 19:29:06 -0500

    0.0.3
    sigue bugeado

---
+++ Monoda vS/Assets/Scripts/gameJoltUserScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameJolt.API;
+
+public class gameJoltUserScript : MonoBehaviour {
+	string user = string.Empty;
+	public Text nombrePruebas;
+	public string User
+	{
+		get
+		{
+			return user;
+		}
+		set
+		{
+			nombrePruebas.text = "Hola, "+value;
+			user = value;
+		}
+	}
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitUntil(() => GameJoltAPI.Instance.HasUser);
+		User = GameJoltAPI.Instance.CurrentUser.Name;
+		//yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
